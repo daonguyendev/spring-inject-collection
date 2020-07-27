7f87c5c62dfa1dcdfe885fc6e0d70fd44b382b14
daonguyen <daonguyen.dev@gmail.com>
2020-07-27 11:27:25 +0700

    Init spring inject collection example

---
+++ src/main/java/com/daonguyen/MainApplication.java
@@ -0,0 +1,16 @@
+package com.daonguyen;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "Beans.xml");
+        JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
+        jc.getAddressList();
+        jc.getAddressSet();
+        jc.getAddressMap();
+        jc.getAddressProp();
+    }
+}
